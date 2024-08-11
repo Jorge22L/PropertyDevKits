307cbd17483919fa10976a45ef2f8456144e68f7
Jorge Morales <jorgeluismreyes@gmail.com>
2024-08-11 16:48:54 -0600

    eliminando html

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,78 @@
-const { src, dest, series, parallel } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss');
-const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
 const cssnano = require('cssnano');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
 const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin'); // Minificar imagenes
+const notify = require('gulp-notify');
+const cache = require('gulp-cache');
 const clean = require('gulp-clean');
 const webp = require('gulp-webp');
 
 const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
-    images: 'src/img/**/*'
-};
+    imagenes: 'src/img/**/*'
+}
+
+function css() {
+    return src(paths.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        // .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
+}
+
+function javascript() {
+    return src(paths.js)
+      .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      .pipe(terser())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(sourcemaps.write('.', {
+        sourceMappingURL: file => `${file.relative}.map`
+       }))
+       .pipe(dest('./build/js'));
+}
+
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(cache(imagemin({ optimizationLevel: 3 })))
+        .pipe(dest('build/img'))
+        .pipe(notify('Imagen Completada' ));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada' }));
+}
+
+
+function watchArchivos() {
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
+}
 
 function cleanBuild() {
     return src('build/*', { read: false, allowEmpty: true })
         .pipe(clean());
 }
 
-function compileCSS() {
-    return src(paths.scss)
-        .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'));
-}
+const build = series(cleanBuild, parallel(css, javascript, imagenes, versionWebp));
 
-function compileJS() {
-    return src(paths.js)
-        .pipe(sourcemaps.init())
-        .pipe(concat('bundle.js'))
-        .pipe(terser())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/js'));
-}
 
-function optimizeImages() {
-    return src(paths.images)
-        .pipe(imagemin([
-            imagemin.mozjpeg({ quality: 75, progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo({
-                plugins: [{ removeViewBox: true }]
-            })
-        ]))
-        .pipe(webp({ quality: 75 }))
-        .pipe(dest('build/img'));
-}
-
-const build = series(cleanBuild, parallel(compileCSS, compileJS, optimizeImages));
-
+exports.css = css;
+exports.watchArchivos = watchArchivos;
+exports.cleanBuild = cleanBuild;
 exports.build = build;
 exports.default = build;
