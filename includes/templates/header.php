2831e1b55fb8229630bd44df123285c0cf6ddbbf
Jorge Morales <jorgeluismreyes@gmail.com>
2024-09-21 09:35:32 -0600

    Agregando funcionalidad php

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,47 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raices</title>
     <link rel="stylesheet" href="build/css/app.css">
 </head>
+
 <body>
-    <header class="header">
+    <header class="header <?php echo $inicio ? 'inicio' : ''; ?>">
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/">
                     <img src="build/img/logo.svg" alt="Logo" />
                 </a>
 
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="menu móvil">
+                </div>
+
                 <div class="derecha">
                     <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
+                        <a href="nosotros.php">Nosotros</a>
                         <a href="anuncios.html">Anuncios</a>
                         <a href="blog.html">Blog</a>
                         <a href="contacto.html">Contacto</a>
                     </nav>
                 </div>
             </div>
-            
+            <h1>Venta de Casas y Apartamentos</h1>
         </div>
     </header>
-
-    <main class="contenedor seccion">
-        <h1>Más Sobre Nosotros</h1>
-    </main>
-
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-        </div>
-    </footer>
-
-    <script src="build/js/bundle.min.js" ></script>
-</body>
-</html>
---
+++ nosotros.php
@@ -0,0 +1,11 @@
+<?php
+include "includes/funciones.php";
+incluirTemplate('header');
+?>
+<main class="contenedor seccion">
+    <h1>Más Sobre Nosotros</h1>
+</main>
+
+<?php
+incluirTemplate('footer');
+?>
